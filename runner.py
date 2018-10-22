8be7f100e8e7c501c812fdf48e518de88d6e25f8
Paul Guimaraes <PGuimaraes@08.big.lan>
2018-10-22 14:50:00 -0300

    Versão inicial.

---
+++ runner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import argparse
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+programName = 'Runner 1.0'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--command', help='Command to execute.', required=True)
+parser.add_argument('-n', '--numberThreads', help='Number threads to execute', type=int, required=True)
+
+try:
+    args = parser.parse_args()
+except:
+    print(programName, flush=True)
+    exit(0)
+
+def run(command):
+    print('Running %s...' % command, flush=True)
+    os.system(command)
+    print('%s finished.' % command, flush=True)
+
+commands = open(args.command, 'r')
+with ThreadPoolExecutor(max_workers=args.numberThreads) as executor:
+    for command in commands:
+        command = command.rstrip()
+        future = executor.submit(run, (command))
