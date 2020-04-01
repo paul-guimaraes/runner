714d6878d0e0df93f2a318a4c5656b966435ecf2
Paul Anderson Souza Guimarães <30377658+paul-guimaraes@users.noreply.github.com>
2020-04-01 18:47:06 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Runner
-Executa comandos do sistema em processos paralelos.
+# Runner 1.01
+Permite a execução de uma fila de programas.
+
+Os comandos são enfileirados em um arquivo de comandos (um por linha).
+
+Use a opção --help para ver a lista de parâmetros.
--- runner.py
+++ runner.py
@@ -1,29 +1,67 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import argparse
+import datetime
+import getpass
 import os
+import pexpect
+import shlex
+import subprocess
+import sys
 from concurrent.futures import ThreadPoolExecutor
 
-programName = 'Runner 1.0'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--command', help='Command to execute.', required=True)
-parser.add_argument('-n', '--numberThreads', help='Number threads to execute', type=int, required=True)
+class Runner:
+    def __init__(self, number_threads, output_directory, password):
+        self.number_threads = number_threads
+        self.output_directory = output_directory
+        self.password = password
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory)
+        self.log_execution = open(os.path.join(self.output_directory, 'execution.log'), 'w')
 
-try:
-    args = parser.parse_args()
-except:
-    print(programName, flush=True)
-    exit(0)
+    def __del__(self):
+        self.log_execution.close()
 
-def run(command):
-    print('Running %s...' % command, flush=True)
-    os.system(command)
-    print('%s finished.' % command, flush=True)
+    def log(self, message):
+        now = datetime.datetime.now()
+        now = now.strftime("%d/%m/%Y %H:%M:%Sh")
+        print('{}: {}'.format(now, message), flush=True)
+        self.log_execution.write('{}: {}\n'.format(now, message))
+        self.log_execution.flush()
 
-commands = open(args.command, 'r')
-with ThreadPoolExecutor(max_workers=args.numberThreads) as executor:
-    for command in commands:
-        command = command.rstrip()
-        future = executor.submit(run, (command))
-commands.close()
+    def run(self, _command, _command_number, password=None):
+        self.log('Executando %s...' % _command)
+        process = pexpect.spawn('/bin/bash -c "{}"'.format(_command.replace('"', "'")), encoding='utf8')
+        if password is not None:
+            process.expect(['[pP]assword', '[sS]enha'])
+            process.sendline(password)
+        output_file = open(os.path.join(self.output_directory, 'command_{}.log'.format(_command_number)), 'w')
+        output_file.write('{}\n\n'.format(_command))
+
+        for line in process.readlines():
+            print(line.strip())
+            output_file.write(line)
+        output_file.close()
+
+    def start(self):
+        password = getpass.getpass("Entre com sua senha por favor:") if self.password else None
+        commands = open(args.command, 'r')
+        with ThreadPoolExecutor(max_workers=self.number_threads) as executor:
+            for i, command in enumerate(commands):
+                command = command.strip()
+                if not command.startswith('#'):
+                    # future = executor.submit(self.run, command, i+1, password)
+                    self.run(command, i+1, password)
+        commands.close()
+
+
+parser = argparse.ArgumentParser('Runner 1.01')
+parser.add_argument('-c', '--command', help='File with command list to execute. One command per line', required=True)
+parser.add_argument('-n', '--numberThreads', help='Number threads to execute commands', type=int, default=1)
+parser.add_argument('-o', '--output', help='Output result directory', default='.')
+parser.add_argument('-p', '--password', help='Fill passwords if requested by commands in execution', action='store_true')
+
+args = parser.parse_args()
+runner = Runner(args.numberThreads, args.output, args.password)
+runner.start()
